c6539b39d4590395173308a42267cef70901dc56
zimbloe <nikolai05ivanov@gmail.com>
2024-02-29 17:08:58 +0200

    Project upload

---
+++ Bank.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+        public class BankAccount
+        {
+            private decimal balance;
+
+            public BankAccount(decimal initialBalance)
+            {
+                if (initialBalance < 0)
+                {
+                    throw new ArgumentException("Initial balance cannot be negative.");
+                }
+
+                balance = initialBalance;
+            }
+
+            public decimal Balance
+            {
+                get { return balance; }
+            }
+
+            public void Deposit(decimal amount)
+            {
+                if (amount < 0)
+                {
+                    throw new ArgumentException("Deposit amount cannot be negative.");
+                }
+
+                balance += amount;
+            }
+
+            public void Withdraw(decimal amount)
+            {
+                if (amount < 0)
+                {
+                    throw new ArgumentException("Withdrawal amount cannot be negative.");
+                }
+
+                if (amount > balance)
+                {
+                    throw new InvalidOperationException("Insufficient funds.");
+                }
+
+                balance -= amount;
+            }
+        }
+
+    }
+
